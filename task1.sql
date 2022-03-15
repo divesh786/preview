3e3f207e4aa8bf0ce51a22d2e9c4ec6abd4ee5b0
dinesh <choudharydivesh4@gmail.com>
2022-03-15 12:52:53 +0530

    task2 update query change

--- task1.sql
+++ task1.sql
@@ -1,9 +1,7 @@
-UPDATE library_posts T
-       JOIN (SELECT *,
-                    Row_number()
-                      OVER(
-                        partition BY library_id
-                        ORDER BY library_id, priority ASC) AS RN
-             FROM   library_posts)T1
-         ON T1.post_id = T.post_id
-SET    T.priority = rn 
+UPDATE library_posts T 
+    JOIN (
+           SELECT *, row_number() over(PARTITION BY library_id ORDER BY library_id,priority ASC) as RN
+           FROM library_posts  
+          )T1
+    ON T1.post_id=T.post_id
+    SET T.priority=RN
